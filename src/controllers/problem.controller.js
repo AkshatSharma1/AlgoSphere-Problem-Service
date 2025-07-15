91caaa9a00916d3b348836bd5c898ed197d34064
Akshat Ajay <akshat.ajay0931@gmail.com>
2025-07-15 22:24:57 +0530

    Feat: Setup Problem Contoller and Routes

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,34 @@
+function pingProblemController(req, res){
+    return res.json({message: "Problem Controller is Working"})
+}
+
+function addProblem(req,res){
+
+}
+
+function getProblem(req, res){
+    
+}
+
+function getAllProblems(req, res){
+    
+}
+
+
+function updateProblem(req, res){
+
+}
+
+function deleteProblem(req, res){
+
+}
+
+
+module.exports = {
+    pingProblemController,
+    addProblem,
+    getProblem,
+    getAllProblems,
+    updateProblem,
+    deleteProblem
+}
