6222a92c05df7bc6849fa1d83e0f0dd4ccd93d6d
Akshat Ajay <akshat.ajay0931@gmail.com>
2025-07-17 00:30:22 +0530

    Feat: Mongo Setup done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const BaseError = require("./errors/base.error");
 const errorHandler = require("./utils/error.handler");
+const connectToDB = require("./config/db.config");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
@@ -26,8 +28,17 @@ app.use(errorHandler);
 
 //Note: if we use this errorHandler lets say on top, and we have 4 params in function errorHandler(err, req,res, next) then its a errorhandler but if it has 3 params then its a normal middleware and it can and cannot act as errorHandler.
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started on port, ${PORT}`);
+
+  await connectToDB();
+  console.log("Connected to DB");
+
+  /* Dummy data
+  const Cat = mongoose.model("Cat", { name: String });
+  const kitty = new Cat({ name: "Kitty Babe" });
+  kitty.save().then(() => console.log("Meow"));
+  */
 
   // throw new BaseError(
   //   "Some Error Occurred",
